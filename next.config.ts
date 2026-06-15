371f3927d479455625c67f8894ca768b8f121e04
RayanKhairullah <raynrayan1341@gmail.com>
2026-06-15 10:31:36 +0700

    fix: deployment github actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Mengubah build menjadi HTML/CSS/JS statis
+  basePath: '/qrcode-custom-generator', // Sesuaikan dengan nama repository GitHub Anda
+  images: {
+    unoptimized: true, // GitHub Pages tidak mendukung optimasi gambar bawaan Next.js
+  },
 };
 
 export default nextConfig;
